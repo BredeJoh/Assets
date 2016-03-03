93f2e045075f6fec5b34a782be74a76658f6652d
BredeJoh <brede_96@hotmail.com>
2016-03-03 23:01:58 +0100

    additions

---
+++ scripts/AddClickPoints.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddClickPoints : MonoBehaviour {
+
+    Text addclickGainText;
+    float cost = 10f;
+    float pointsAdded = 0f;
+    float PointsAddNext = 1f;
+
+    // Use this for initialization
+    void Start () {
+        addclickGainText = GameObject.Find("addText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Points.points >= cost)
+        {
+            Points.points -= cost;
+            pointsAdded = PointsAddNext;
+            Points.clickGain += pointsAdded;
+            PointsAddNext *= 2f;
+            cost *= 3f;
+        }
+	    addclickGainText.text = "Cost: " + cost + "\n+" + PointsAddNext + " per click";
+        gameObject.SetActive(false);
+
+    }
+}
---
+++ scripts/Points.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+
+    public static float points = 0f;
+    public static float clickGain = 1f;
+    float multiplier = 1f;
+    Text pointsText;
+    
+
+    // Use this for initialization
+    void Start () {
+        pointsText = GameObject.Find("PointsInfo").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pointsText.text = "Points: " + Points.points + "\nPoints per click: " + clickGain;
+        clickGain = clickGain * multiplier;
+        
+	}
+}
